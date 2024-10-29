b370754fcce575cf4b82631efd1e5eca3f6dad72
mvargas44 <mvargas44@cuc.edu.co>
2024-10-29 13:17:47 -0500

    administrar con iconos

--- src/app/componentes/canciones/CancionAdmin.tsx
+++ src/app/componentes/canciones/CancionAdmin.tsx
@@ -1,8 +1,82 @@
-export const CancionAdmin = () =>{
-    return(
+export const CancionAdmin = () => {
+    return (
         <>
-        Canción Admin <br />
-        Canciones...
+            <div className="pt-5 d-flex justify-content-center">
+                <div className="col-md-8">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Título Canción</th>
+                                <th>Cantante</th>
+                                <th>Duración</th>
+                                <th>Género</th>
+                                <th>Opciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>La noche</td>
+                                <td>Joe Arroyo</td>
+                                <td>4.5</td>
+                                <td>Salsa</td>
+                                <td>
+                                    <a href="/canact/1">
+                                        <i className="fa-solid fa-pen-to-square" style={{ color: "#000080" }}></i>
+                                    </a>
+                                    &nbsp;
+                                    <i className="fa-solid fa-trash" style={{ color: "#000080" }}></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Mi ex tenía razón</td>
+                                <td>Karol G</td>
+                                <td>3.5</td>
+                                <td>Reguetón</td>
+                                <td>
+                                    <a href="/canact/2">
+                                        <i className="fa-solid fa-pen-to-square" style={{ color: "#000080" }}></i>
+                                    </a>
+                                    &nbsp;
+                                    <i className="fa-solid fa-trash" style={{ color: "#000080" }}></i></td>
+                            </tr>
+
+                            <tr>
+                                <td>3</td>
+                                <td>Sin medir distancia</td>
+                                <td>Diomedes Díaz</td>
+                                <td>4.8</td>
+                                <td>Vallenato</td>
+                                <td>
+                                    <a href="/canact/3">
+                                        <i className="fa-solid fa-pen-to-square" style={{ color: "#000080" }}></i>
+                                    </a>
+                                    &nbsp;
+                                    <i className="fa-solid fa-trash" style={{ color: "#000080" }}></i>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>4</td>
+                                <td>My Immortal</td>
+                                <td>Evanescence</td>
+                                <td>4.3</td>
+                                <td>Rock</td>
+                                <td>
+                                    <a href="/canact/4">
+                                        <i className="fa-solid fa-pen-to-square" style={{ color: "#000080" }}></i>
+                                    </a>
+                                    &nbsp;
+                                    <i className="fa-solid fa-trash" style={{ color: "#000080" }}></i>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </>
     )
 }
--- src/app/componentes/canciones/CancionCrear.tsx
+++ src/app/componentes/canciones/CancionCrear.tsx
@@ -1,8 +1,79 @@
-export const CancionCrear = () =>{
-    return(
-        <>
-        Canción Crear <br />
-        Canciones...
-        </>
-    )
-}
+export const CancionCrear = () => {
+    return (
+      <>
+      <div className="pt-5 d-flex justify-content-center">
+        <div className="col-md-8">
+   
+        <form className="row g-3 needs-validation">
+         
+          <div className="col-md-6 position-relative">
+            <label htmlFor="validationTooltip01" className="form-label">
+              Título Canción
+            </label>
+            <input
+              type="text"
+              className="form-control border-info"
+              id="titu"
+              name="titu"
+              required
+            />
+           
+          </div>
+   
+          <div className="col-md-6 position-relative">
+            <label htmlFor="validationTooltip02" className="form-label">
+              Cantante
+            </label>
+            <input
+              type="text"
+              className="form-control border-info"
+              id="cant"
+              name="cant"
+              required
+            />
+           
+          </div>
+   
+          <div className="col-md-6 position-relative">
+            <label htmlFor="validationTooltip03" className="form-label border-info">
+              Duración
+            </label>
+            <input
+              type="text"
+              className="form-control border-info"
+              id="dura"
+              name="dura"
+              required
+            />
+           
+          </div>
+         
+          <div className="col-md-6 position-relative">
+            <label htmlFor="validationTooltip04" className="form-label border-info">
+              Género
+            </label>
+            <select className="form-select border-info" id="gene" name="gene" required>
+              <option selected disabled value="">
+                Seleccione el género
+              </option>
+              <option>Salsa</option>
+              <option>Vallenato</option>
+              <option>Champeta</option>
+              <option>Reguetón</option>
+              <option>Pop</option>
+            </select>
+           
+          </div>
+         
+          <div className="col-12">
+            <button className="btn btn-primary" type="submit">
+              Registrar canción
+            </button>
+          </div>
+        </form>
+   
+        </div>
+        </div>
+      </>
+    );
+};  
