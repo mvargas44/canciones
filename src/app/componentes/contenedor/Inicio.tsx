74e236b3322f6c07be5ace51679a52e100263c2b
mvargas44 <mvargas44@cuc.edu.co>
2024-11-17 17:46:53 -0500

    Taller Front Completo + CRED - Listar

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -12,6 +12,8 @@ export const Inicio = () => {
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 3"></button>
                 </div>
 
                 <div className="carousel-inner">
